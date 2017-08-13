c96293467b6ef0bc8b0b0584bb533acbed280b1b
fanchj1 <fanchj1@yonyou.com>
2017-08-13 17:04:53 +0800

    change

--- xhsd/src/main/webapp/pages/demo_pf/meta.js
+++ xhsd/src/main/webapp/pages/demo_pf/meta.js
@@ -1,157 +1,87 @@
 var KHxxVOmeta = {
-	params : {
-		"cls" : "com.yonyou.iuap.example.entity.KHxxVO"
-	},
+    params: {
+        "cls": "com.yonyou.iuap.example.entity.KHxxVO"
+    },
 
-	meta : {
-		id_khxx : {
+    meta: {
+        id_khxx: {},
+        khbh: {},
+        khmc: {},
+        kyzt: {},
+        zdsl: {},
 
-		},
-		khbh : {
-
-		},
-		khmc : {
-
-		},
-		kyzt : {
-
-		},
-		zdsl : {
-
-		},
-
-	},
-	pageSize : 5,
-	// 启用前端缓存
-	pageCache : true
+    },
+    pageSize: 5,
+    // 启用前端缓存
+    pageCache: true
 }
 
 var BdxxVOmeta = {
-	params : {
-		"cls" : "com.yonyou.iuap.example.entity.BdxxVO"
-	},
+    params: {
+        "cls": "com.yonyou.iuap.example.entity.BdxxVO"
+    },
 
-	meta : {
-		id_bdxx : {
-
-		},
-		ywbm : {
-
-		},
-		zddwbm : {
-
-		},
-		zddwbmName: {},
-		zddh : {
-
-		},
-		pzbm : {
-
-		},
-		pzbmName: {},
-		zdsl : {
-
-		},
-		fhsl : {
-
-		},
-		fhzk : {
-
-		},
-		zkbj : {
-
-		},
-		lrrq : {
-
-		},
-		clrq : {
-
-		},
-		bz : {
-
-		},
-	},
-	pageSize : 5,
-	// 启用前端缓存
-	pageCache : true
+    meta: {
+        id_bdxx: {},
+        ywbm: {},
+        zddwbm: {},
+        zddwbmName: {},
+        zddh: {},
+        pzbm: {},
+        pzbmName: {},
+        zdsl: {},
+        fhsl: {},
+        fhzk: {},
+        zkbj: {},
+        lrrq: {},
+        clrq: {},
+        bz: {},
+    },
+    pageSize: 5,
+    // 启用前端缓存
+    pageCache: true
 }
 
 var FHVOmeta = {
-	params : {
-		"cls" : "com.yonyou.iuap.example.entity.FHVO"
-	},
+    params: {
+        "cls": "com.yonyou.iuap.example.entity.FHVO"
+    },
 
-	meta : {
-		id_fhb : {
-
-		},
-		ywbm : {
-
-		},
-		zddwbm : {
-
-		},
-		zddh : {
-
-		},
-		pzbm : {
-
-		},
-		fhsl : {
-
-		},
-		fhzk : {
-
-		},
-		fdrq : {
-
-		},
-		fdr : {
-
-		},
-		clzt : {
-
-		},
-		bz : {
-
-		},
-	},
-	pageSize : 5,
-	// 启用前端缓存
-	pageCache : true
+    meta: {
+        id_fhb: {},
+        ywbm: {},
+        zddwbm: {},
+        zddh: {},
+        pzbm: {},
+        fhsl: {},
+        fhzk: {},
+        fdrq: {},
+        fdr: {},
+        clzt: {},
+        bz: {},
+    },
+    pageSize: 5,
+    // 启用前端缓存
+    pageCache: true
 }
 
 var PZxxVOmeta = {
-	params : {
-		"cls" : "com.yonyou.iuap.example.entity.PZxxVO"
-	},
+    params: {
+        "cls": "com.yonyou.iuap.example.entity.PZxxVO"
+    },
 
-	meta : {
-		id_pzxx : {
-
-		},
-		pzbm : {
-
-		},
-		isbn : {
-
-		},
-		sm : {
-
-		},
-		dj : {
-
-		},
-		cbrq : {
-
-		},
-		ysny : {
-
-		},
-		zdsl:{},
-        kcsl:{},
-	},
-	pageSize : 5,
-	// 启用前端缓存
-	pageCache : true
+    meta: {
+        id_pzxx: {},
+        pzbm: {},
+        isbn: {},
+        sm: {},
+        dj: {},
+        cbrq: {},
+        ysny: {},
+        zdsl: {},
+        kcsl: {},
+    },
+    pageSize: 5,
+    // 启用前端缓存
+    pageCache: true
 }
